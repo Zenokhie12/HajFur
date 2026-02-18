b0a6ab737c5ea11f940e8582e9cb4c653970f1f4
Zenokhie12 <Zenokhieasgar1@gmail.com>
2026-02-18 08:51:56 -0800

    Added PWA and Vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Hajar Furniture',
+      short_name: 'HarFur',
+      theme_color: '#800000',
+      display: 'standalone',
+      icons: [
+        { src: 'Hajfur-192x192.png', sizes: '192x192', type: 'image/png' },
+        { src: 'Hajfur-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+      ]
+      }
+  })],
 })
